a62c792046c5f93f446399461d2ca98fe82ea34e
zz134 <zz134@duke.edu>
2019-02-06 00:12:51 -0500

    registration request

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React, { Component } from 'react';
-import { TopBar } from './TopBar'
+import { TopBar } from './TopBar';
+import { Main } from './Main';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <TopBar/>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="App">
+                <TopBar/>
+                <Main/>
+            </div>
+        );
+    }
 }
 
 export default App;
